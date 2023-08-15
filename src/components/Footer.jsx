549234f9629812ac4a617c59e605189f8e3ce949
itsteatv <rezashow60@gmail.com>
2023-08-15 17:44:33 +0330

    small change in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ function Footer() {
         <footer className="bg-white fixed bottom-0 w-full" aria-labelledby="footer-heading">
             <h2 id="footer-heading" className="sr-only">Footer</h2>
 
-            <div className="px-4 py-12 mx-auto bg-gray-50 max-w-7xl sm:px-6 lg:px-16">
+            <div className="px-4 py-5 mx-auto max-w-7xl sm:px-6 lg:px-16">
                 <div className="flex flex-wrap items-baseline justify-center lg:justify-center">
-                    <span className="mt-2 text-sm font-light text-gray-500">
+                    <span className="text-sm font-light text-gray-500">
                         Copyright © 2023 - 2024
                         <a href="https://wickedlabs.dev" className="mx-2 text-wickedblue hover:text-gray-500" rel="noopener noreferrer">@itsteatv</a>. Since 2023
                     </span>

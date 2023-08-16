7aae835dbe30d2a3b4d1e81fd67ae4f78994ea12
itsteatv <rezashow60@gmail.com>
2023-08-16 15:43:15 +0330

    dark mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,85 @@
 import "./index.css"
 import Input from "./components/Input"
 import Footer from "./components/Footer"
+import { useEffect, useState } from "react"
 
 function App() {
+
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "system");
+  const darkQuery = window.matchMedia("(prefers-color-scheme: dark)")
+  console.log(darkQuery);
+
+  const element = document.documentElement;
+
+  const onWindowMatch = function () {
+    if (localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && darkQuery.matches)
+    ) {
+      element.classList.add("dark");
+    } else {
+      element.classList.remove("dark");
+    }
+  }
+
+  onWindowMatch();
+
+  useEffect(() => {
+
+    if (theme === 'dark') {
+      element.classList.add('dark');
+      element.classList.remove('light');
+      localStorage.setItem('theme', 'dark');
+    } else if (theme === 'light') {
+      element.classList.add('light');
+      element.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+    } else {
+      localStorage.removeItem('theme');
+      onWindowMatch();
+    }
+
+
+  }, [theme, onWindowMatch]);
+
+  darkQuery.addEventListener("change", (event) => {
+    if (!("theme" in localStorage)) {
+      if (event.matches) {
+        element.classList.add('dark');
+      } else {
+        element.classList.remove('dark');
+      }
+    }
+  })
+
+  const options = [
+    {
+      icon: "sunny-outline",
+      text: "light",
+    },
+    {
+      icon: "moon-outline",
+      text: "dark",
+    },
+    {
+      icon: "desktop-outline",
+      text: "system",
+    },
+  ]
+
   return (
-    <>
+    <main className="h-screen dark:bg-slate-900 dark:text-gray-100 duration-100">
+      <div className="fixed top-5 right-10 dark:bg-slate-800 bg-gray-100 rounded">
+        {
+          options?.map((option) => (
+            <button onClick={() => setTheme(option.text)} key={option.text} className={`w-8 h-8 leading-9 text-xl rounded-full m-1 ${theme === option.text && "text-sky-600"}`}>
+              <ion-icon name={option.icon}></ion-icon>
+            </button>
+          ))
+        }
+      </div>
       <Input />
       <Footer />
-    </>
+    </main>
   )
 }
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,31 +3,31 @@ function Input() {
     return (
         <section className="">
             <div className=" items-center px-5 py-12 lg:px-20">
-                <div className="flex flex-col w-full max-w-md p-10 mx-auto my-6 transition duration-500 ease-in-out transhtmlForm bg-white rounded-lg md:mt-0">
+                <div className="flex flex-col w-full max-w-md p-10 mx-auto my-6 transition duration-500 ease-in-out transhtmlForm dark:bg-slate-900 dark:text-gray-100 rounded-lg md:mt-0">
                     <div className="mt-8">
                         <div className="mt-6">
-                            <htmlForm className="space-y-6">
+                            <div className="space-y-6">
                                 <div>
                                     <label htmlFor="product" className="block text-sm font-medium text-neutral-600"> Product & Price </label>
                                     <div className="mt-1">
-                                        <input id="product" name="product" type="text" required="" placeholder="+100 laptop" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"></input>
+                                        <input id="product" name="product" type="text" required="" placeholder="+100 laptop" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-gray-100"></input>
                                     </div>
                                 </div>
 
                                 <div>
                                     <label htmlFor="data" className="block text-sm font-medium text-neutral-600"> Product & Price </label>
                                     <div className="mt-1">
-                                        <input id="data" name="data" type="datetime-local" required="" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"></input>
+                                        <input id="data" name="data" type="datetime-local" required="" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 dark:bg-slate-800 dark:text-gray-100"></input>
                                     </div>
                                 </div>
 
                                 <div className="space-y-1">
                                     <label htmlFor="description" className="block text-sm font-medium text-neutral-600"> Description </label>
                                     <div className="mt-1">
-                                        <input id="description" name="description" type="text" required="" placeholder="description" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"></input>
+                                        <input id="description" name="description" type="text" required="" placeholder="description" className="block w-full px-5 py-3 text-base text-neutral-600 placeholder-gray-300 transition duration-500 ease-in-out transhtmlForm border border-transparent rounded-lg bg-gray-50 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 dark:bg-slate-800 dark:text-gray-100 dark:placeholder-pink-100"></input>
                                     </div>
                                 </div>
-                            </htmlForm>
+                            </div>
                         </div>
                     </div>
                 </div>
